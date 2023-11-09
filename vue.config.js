bd8ddc0e2db2e70a2ff95ecef24af62b8c49106c
Karma <quiteplacedev@gmail.com>
2023-11-09 10:07:36 +0500

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  publicPath: '/static/src/dist/',
+  outputDir: path.resolve(__dirname, '../backend/static/src/dist/'),
+  filenameHashing: false,
+  runtimeCompiler: true,
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+  configureWebpack: {
+    output: {
+      clean: true,
+    },
+    plugins: [new CleanWebpackPlugin()],
+  },
+};
